484a508f8a62ba83aaa260f081b5226ac8dd01aa
Vishal Guleria <vishalguleria9816@gmail.com>
2023-12-02 19:18:40 +0530

    BAsic layout
    Signed-off-by: Vishal Guleria <vishalguleria9816@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,23 +3,95 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tic-tac-toe">
+        <h1> TIC-TAC-TOE </h1>
+        <Button resetGame={resetGame} />
+        <div className="game">
+            {Array.from("012345678").map((ind) => (
+                <Square
+                    key={ind}
+                    ind={ind}
+                    updateSquares={updateSquares}
+                    clsName={squares[ind]}
+                />
+            ))}
+        </div>
+        <div className={`turn ${turn === "x" ? "left" : "right"}`}>
+            <Square clsName="x" />
+            <Square clsName="o" />
+        </div>
+        <AnimatePresence>
+            {winner && (
+                <motion.div
+                    key={"parent-box"}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="winner"
+                >
+                    <motion.div
+                        key={"child-box"}
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0, opacity: 0 }}
+                        className="text"
+                    >
+                        <motion.h2
+                            initial={{ scale: 0, y: 100 }}
+                            animate={{
+                                scale: 1,
+                                y: 0,
+                                transition: {
+                                    y: { delay: 0.7 },
+                                    duration: 0.7,
+                                },
+                            }}
+                        >
+                            {winner === "x | o"
+                                ? "No Winner :/"
+                                : "Win !! :)"}
+                        </motion.h2>
+                        <motion.div
+                            initial={{ scale: 0 }}
+                            animate={{
+                                scale: 1,
+                                transition: {
+                                    delay: 1.3,
+                                    duration: 0.2,
+                                },
+                            }}
+                            className="win"
+                        >
+                            {winner === "x | o" ? (
+                                <>
+                                    <Square clsName="x" />
+                                    <Square clsName="o" />
+                                </>
+                            ) : (
+                                <>
+                                    <Square clsName={winner} />
+                                </>
+                            )}
+                        </motion.div>
+                        <motion.div
+                            initial={{ scale: 0 }}
+                            animate={{
+                                scale: 1,
+                                transition: { delay: 1.5, duration: 0.3 },
+                            }}
+                        >
+                            <Button resetGame={resetGame} />
+                        </motion.div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     </div>
   );
 }
+
+const Button = ({ resetGame }) => {
+  return <button onClick={() => resetGame()}>New Game</button>;
+};
 
 export default App;
